d97db0496731a19e0a2fcb8b349cc80271deb38e
wkwksama <mailpakgasing@gmail.com>
2017-09-20 05:11:15 +0700

    get data

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,12 @@
                     <label>Mail</label>
                     <input type="text" class="form-control" v-model="user.email">
                 </div>
-                <button @click="submit" class="btn btn-primary">Submit</button>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users"> {{ u.username }} - {{ u.email }} </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -24,16 +29,30 @@
                 user: {
                     username: '',
                     email: ''
-                }
+                },
+                users: []
             }
         },
         methods: {
             submit () {
-                this.$http.post('https://belajar-vue-http.firebaseio.com/data.json', this.user)
+                this.$http.post('', this.user)
                     .then(response => {
                         console.log(response)
                     }, error => {
                         console.log(error)
+                    })
+            },
+            fetchData () {
+                this.$http.get('')
+                    .then(response => {
+                        return response.json()
+                    })
+                    .then(data => {
+                        const resultArray = []
+                        for (let key in data) {
+                            resultArray.push(data[key])
+                        }
+                        this.users = resultArray
                     })
             }
         }
